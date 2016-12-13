58321c9e1a858d4c56fcfd2cd3500fa96951e363
Roman_Charnashe <Roman_Charnashei@epam.com>
2016-12-13 20:45:11 +0300

    replication done

---
+++ mongo_shell/replication_run.bat
@@ -0,0 +1,3 @@
+start cmd /k mongod --replSet testApp --dbpath "./data/node1" --port 40000
+start cmd /k mongod --replSet testApp --dbpath "./data/node2" --port 40001
+start cmd /k mongod --replSet testApp --dbpath "./data/arbiter" --port 40002
